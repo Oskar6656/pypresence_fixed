71bb87b5144728004e52db3620f497fc057f5e70
Oskar6656 <105849178+Oskar6656@users.noreply.github.com>
2024-01-07 09:45:14 +0300

    Add files via upload

--- pypresence/__init__.py
+++ pypresence/__init__.py
@@ -6,12 +6,44 @@ By: qwertyquerty and LewdNeko
 
 from .baseclient import BaseClient
 from .client import Client, AioClient
-from .exceptions import *
+from .exceptions import (
+    DiscordError,
+    InvalidArgument,
+    InvalidID,
+    InvalidPipe,
+    PipeClosed,
+    PyPresenceException,
+    ResponseTimeout,
+    ArgumentError,
+    ConnectionTimeout,
+    DiscordNotFound,
+    EventNotFound,
+    ServerError,
+)
 from .presence import Presence, AioPresence
 
 
-__title__ = 'pypresence'
-__author__ = 'qwertyquerty'
-__copyright__ = 'Copyright 2018 - Current qwertyquerty'
-__license__ = 'MIT'
-__version__ = '4.3.0'
+__title__ = "pypresence"
+__author__ = "qwertyquerty"
+__copyright__ = "Copyright 2018 - Current qwertyquerty"
+__license__ = "MIT"
+__version__ = "4.3.0"
+__all__ = (
+    "BaseClient",
+    "Client",
+    "AioClient",
+    "Presence",
+    "AioPresence",
+    "DiscordError",
+    "InvalidArgument",
+    "InvalidID",
+    "InvalidPipe",
+    "PipeClosed",
+    "PyPresenceException",
+    "ResponseTimeout",
+    "ArgumentError",
+    "ConnectionTimeout",
+    "DiscordNotFound",
+    "EventNotFound",
+    "ServerError",
+)
--- pypresence/payloads.py
+++ pypresence/payloads.py
@@ -7,7 +7,6 @@ from .utils import remove_none
 
 
 class Payload:
-
     def __init__(self, data, clear_none=True):
         if clear_none:
             data = remove_none(data)
@@ -21,62 +20,60 @@ class Payload:
         return time.time()
 
     @classmethod
-    def set_activity(cls, pid: int = os.getpid(),
-                     state: str = None, details: str = None,
-                     start: int = None, end: int = None,
-                     large_image: str = None, large_text: str = None,
-                     small_image: str = None, small_text: str = None,
-                     party_id: str = None, party_size: list = None,
-                     join: str = None, spectate: str = None,
-                     match: str = None, buttons: list = None,
-                     instance: bool = True, activity: Union[bool, None] = True,
-                     _rn: bool = True):
+    def set_activity(
+        cls,
+        pid: int = os.getpid(),
+        state: str = None,
+        details: str = None,
+        start: int = None,
+        end: int = None,
+        large_image: str = None,
+        large_text: str = None,
+        small_image: str = None,
+        small_text: str = None,
+        party_id: str = None,
+        party_size: list = None,
+        join: str = None,
+        spectate: str = None,
+        match: str = None,
+        buttons: list = None,
+        instance: bool = True,
+        activity: Union[bool, None] = True,
+        _rn: bool = True,
+    ):
 
         # They should already be an int because we give typehints, but some people are fucking stupid and use
         # IDLE or some other stupid shit.
         if start:
-            start = int(start)
+            start = start
         if end:
-            end = int(end)
+            end = end
 
         if activity is None:
             act_details = None
             clear = True
         else:
             act_details = {
-                    "state": state,
-                    "details": details,
-                    "timestamps": {
-                        "start": start,
-                        "end": end
-                    },
-                    "assets": {
-                        "large_image": large_image,
-                        "large_text": large_text,
-                        "small_image": small_image,
-                        "small_text": small_text
-                    },
-                    "party": {
-                        "id": party_id,
-                        "size": party_size
-                    },
-                    "secrets": {
-                        "join": join,
-                        "spectate": spectate,
-                        "match": match
-                    },
-                    "buttons": buttons,
-                    "instance": instance
-                }
+                "state": state,
+                "details": details,
+                "timestamps": {"start": start, "end": end},
+                "assets": {
+                    "large_image": large_image,
+                    "large_text": large_text,
+                    "small_image": small_image,
+                    "small_text": small_text,
+                },
+                "party": {"id": party_id, "size": party_size},
+                "secrets": {"join": join, "spectate": spectate, "match": match},
+                "buttons": buttons,
+                "instance": instance,
+            }
             clear = False
 
         payload = {
             "cmd": "SET_ACTIVITY",
-            "args": {
-                "pid": pid,
-                "activity": act_details
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"pid": pid, "activity": act_details},
+            "nonce": "{:.20f}".format(cls.time()),
         }
         if _rn:
             clear = _rn
@@ -86,11 +83,8 @@ class Payload:
     def authorize(cls, client_id: str, scopes: List[str]):
         payload = {
             "cmd": "AUTHORIZE",
-            "args": {
-                "client_id": str(client_id),
-                "scopes": scopes
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"client_id": client_id, "scopes": scopes},
+            "nonce": "{:.20f}".format(cls.time()),
         }
         return cls(payload)
 
@@ -98,10 +92,8 @@ class Payload:
     def authenticate(cls, token: str):
         payload = {
             "cmd": "AUTHENTICATE",
-            "args": {
-                "access_token": token
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"access_token": token},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -110,9 +102,8 @@ class Payload:
     def get_guilds(cls):
         payload = {
             "cmd": "GET_GUILDS",
-            "args": {
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -121,10 +112,8 @@ class Payload:
     def get_guild(cls, guild_id: str):
         payload = {
             "cmd": "GET_GUILD",
-            "args": {
-                "guild_id": str(guild_id),
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"guild_id": guild_id},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -133,10 +122,8 @@ class Payload:
     def get_channels(cls, guild_id: str):
         payload = {
             "cmd": "GET_CHANNELS",
-            "args": {
-                "guild_id": str(guild_id),
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"guild_id": guild_id},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -145,30 +132,30 @@ class Payload:
     def get_channel(cls, channel_id: str):
         payload = {
             "cmd": "GET_CHANNEL",
-            "args": {
-                "channel_id": str(channel_id),
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"channel_id": channel_id},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
 
     @classmethod
-    def set_user_voice_settings(cls, user_id: str, pan_left: float = None,
-                                pan_right: float = None, volume: int = None,
-                                mute: bool = None):
+    def set_user_voice_settings(
+        cls,
+        user_id: str,
+        pan_left: float = None,
+        pan_right: float = None,
+        volume: int = None,
+        mute: bool = None,
+    ):
         payload = {
             "cmd": "SET_USER_VOICE_SETTINGS",
             "args": {
-                "user_id": str(user_id),
-                "pan": {
-                    "left": pan_left,
-                    "right": pan_right
-                },
+                "user_id": user_id,
+                "pan": {"left": pan_left, "right": pan_right},
                 "volume": volume,
-                "mute": mute
+                "mute": mute,
             },
-            "nonce": '{:.20f}'.format(cls.time())
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload, True)
@@ -177,10 +164,8 @@ class Payload:
     def select_voice_channel(cls, channel_id: str):
         payload = {
             "cmd": "SELECT_VOICE_CHANNEL",
-            "args": {
-                "channel_id": str(channel_id),
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"channel_id": channel_id},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -189,9 +174,8 @@ class Payload:
     def get_selected_voice_channel(cls):
         payload = {
             "cmd": "GET_SELECTED_VOICE_CHANNEL",
-            "args": {
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -200,10 +184,8 @@ class Payload:
     def select_text_channel(cls, channel_id: str):
         payload = {
             "cmd": "SELECT_TEXT_CHANNEL",
-            "args": {
-                "channel_id": str(channel_id),
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"channel_id": channel_id},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -216,7 +198,7 @@ class Payload:
             "cmd": "SUBSCRIBE",
             "args": args,
             "evt": event.upper(),
-            "nonce": '{:.20f}'.format(cls.time())
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -229,7 +211,7 @@ class Payload:
             "cmd": "UNSUBSCRIBE",
             "args": args,
             "evt": event.upper(),
-            "nonce": '{:.20f}'.format(cls.time())
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -238,19 +220,26 @@ class Payload:
     def get_voice_settings(cls):
         payload = {
             "cmd": "GET_VOICE_SETTINGS",
-            "args": {
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
 
     @classmethod
-    def set_voice_settings(cls, _input: dict = None, output: dict = None,
-                           mode: dict = None, automatic_gain_control: bool = None,
-                           echo_cancellation: bool = None, noise_suppression: bool = None,
-                           qos: bool = None, silence_warning: bool = None,
-                           deaf: bool = None, mute: bool = None):
+    def set_voice_settings(
+        cls,
+        _input: dict = None,
+        output: dict = None,
+        mode: dict = None,
+        automatic_gain_control: bool = None,
+        echo_cancellation: bool = None,
+        noise_suppression: bool = None,
+        qos: bool = None,
+        silence_warning: bool = None,
+        deaf: bool = None,
+        mute: bool = None,
+    ):
         payload = {
             "cmd": "SET_VOICE_SETTINGS",
             "args": {
@@ -263,9 +252,9 @@ class Payload:
                 "qos": qos,
                 "silence_warning": silence_warning,
                 "deaf": deaf,
-                "mute": mute
+                "mute": mute,
             },
-            "nonce": '{:.20f}'.format(cls.time())
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload, True)
@@ -274,10 +263,8 @@ class Payload:
     def capture_shortcut(cls, action: str):
         payload = {
             "cmd": "CAPTURE_SHORTCUT",
-            "args": {
-                "action": action.upper()
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"action": action.upper()},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -286,10 +273,8 @@ class Payload:
     def send_activity_join_invite(cls, user_id: str):
         payload = {
             "cmd": "SEND_ACTIVITY_JOIN_INVITE",
-            "args": {
-                "user_id": str(user_id)
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"user_id": user_id},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
@@ -298,10 +283,8 @@ class Payload:
     def close_activity_request(cls, user_id: str):
         payload = {
             "cmd": "CLOSE_ACTIVITY_REQUEST",
-            "args": {
-                "user_id": str(user_id)
-            },
-            "nonce": '{:.20f}'.format(cls.time())
+            "args": {"user_id": user_id},
+            "nonce": "{:.20f}".format(cls.time()),
         }
 
         return cls(payload)
